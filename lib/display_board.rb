f4f26a1e25a371ec2ec85e36f28144e4e277818e
achesky <afc340@nyu.edu>
2016-01-16 14:27:35 -0500

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,26 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+  puts " O | X | O "
+  puts " O | X | X "
+  puts " X | O | X "
+  puts " X | O | O "
+  puts "   | O |   "
+  puts "   |   | O "
+  puts "   |   | X "
+  puts " X |   |   "
+  puts " O | O | O "
+  puts " X | X | X "
+  puts " O |   |   "
+  puts "   | X |   "
+  puts "   |   |   "
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "-----------"
+  puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts "-----------"
+  puts "#{board[6]}|#{board[7]}|#{board[8]}"
+end
+
+display_board(board)
